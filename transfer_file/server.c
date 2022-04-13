4bd72cb356983a81b8b3707cc2673eb50d4f838a
Nga Nguyen <ngant@Ngas-MacBook-Pro.local>
2022-04-13 13:41:15 +0700

    tranfer

--- transfer_file/server.c
+++ transfer_file/server.c
@@ -1,125 +1,181 @@
-// #include <stdlib.h>
-// #include <stdio.h>
-// #include <sys/types.h>
-// #include <sys/socket.h>
-// #include <netinet/in.h>
-// #include <string.h>
-// #include <unistd.h>
-// #include <fcntl.h>
-// #include <arpa/inet.h>
 
-// #define MAXLINE 4096 /*max text line length*/
-// #define SERV_PORT 3000 /*port*/
-// #define LISTENQ 8 /*maximum number of client connections */
-
-// int main (int argc, char **argv)
-// {
-//  int listenfd, connfd, n;
-//  pid_t childpid;
-//  socklen_t clilen;
-//  char buf[MAXLINE];
-//  struct sockaddr_in cliaddr, servaddr;
-	
-//  //creation of the socket
-//  listenfd = socket (AF_INET, SOCK_STREAM, 0);
-	
-//  //preparation of the socket address 
-//  servaddr.sin_family = AF_INET;
-//  servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-//  servaddr.sin_port = htons(SERV_PORT);
-	
-//  bind (listenfd, (struct sockaddr *) &servaddr, sizeof(servaddr));
-	
-//  listen (listenfd, LISTENQ);
-	
-//  printf("%s\n","Server running...waiting for connections.");
-	
-//  for ( ; ; ) {
-		
-//   clilen = sizeof(cliaddr);
-//   connfd = accept (listenfd, (struct sockaddr *) &cliaddr, &clilen);
-//   printf("%s\n","Received request...");
-				
-//   while ( (n = recv(connfd, buf, MAXLINE,0)) > 0)  {
-//    printf("%s","String received from and resent to the client:");
-//    puts(buf);
-//    send(connfd, buf, n, 0);
-//   }
-			
-//  if (n < 0) {
-//   perror("Read error"); 
-//   exit(1);
-//  }
-//  close(connfd);
-	
-//  }
-//  //close listening socket
-//  close (listenfd); 
-// }
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <stdio.h>          /* These are the usual header files */
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 #include <arpa/inet.h>
-#define SIZE 1024
+#include <string.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <sys/stat.h>
 
-void write_file(int sockfd){
-  int n;
-  FILE *fp;
-  char *filename = "recv.txt";
-  char buffer[SIZE];
+#define PORT 5550   /* Port that will be opened */ 
+#define BACKLOG 2   /* Number of allowed connections */
+#define BUFF_SIZE 1024
+#define MAX_SIZE 10e6 * 100
+#define STORAGE "./file/" //default save file place
+#define EXISTEDFILE "Error: File is existent on server"
+#define BUFF_SEND 1024
 
-  fp = fopen(filename, "w");
-  while (1) {
-    n = recv(sockfd, buffer, SIZE, 0);
-    if (n <= 0){
-      break;
-      return;
-    }
-    fprintf(fp, "%s", buffer);
-    bzero(buffer, SIZE);
-  }
-  return;
+/*
+* Check valid port number
+* @param int port
+* @return 1 if valid port number, else return 0
+*/
+int validPortNumber(int port) {
+	return (port > 0) && (port <= 65535);
 }
+/*
+* Check valid messages
+* @param char* mess
+* @return 1 if valid, else return 0
+*/
 
-int main(){
-  char *ip = "127.0.0.1";
-  int port = 8080;
-  int e;
+int main(int argc, char **argv)
+{
+ 	int port_number;
+ 	if(argc != 2) {
+ 		perror(" Error Parameter! Please input only port number\n ");
+ 		exit(0);
+ 	}
+ 	if((port_number = atoi(argv[1])) == 0) {
+ 		perror(" Please input port number\n");
+ 		exit(0);
+ 	}
+ 	if(!validPortNumber(port_number)) {
+ 		perror("Invalid Port Number!\n");
+ 		exit(0);
+ 	}
 
-  int sockfd, new_sock;
-  struct sockaddr_in server_addr, new_addr;
-  socklen_t addr_size;
-  char buffer[SIZE];
+	int listen_sock, conn_sock; /* file descriptors */
+	char fileName[BUFF_SIZE];
+	char *fileContent;
+	int bytes_sent, bytes_received;
+	struct sockaddr_in server; /* server's address information */
+	struct sockaddr_in client; /* client's address information */
+	int sin_size;
+	FILE *fileptr;
+	long filelen;
 
-  sockfd = socket(AF_INET, SOCK_STREAM, 0);
-  if(sockfd < 0) {
-    perror("[-]Error in socket");
-    exit(1);
-  }
-  printf("[+]Server socket created successfully.\n");
-
-  server_addr.sin_family = AF_INET;
-  server_addr.sin_port = port;
-  server_addr.sin_addr.s_addr = inet_addr(ip);
-
-  e = bind(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr));
-  if(e < 0) {
-    perror("[-]Error in bind");
-    exit(1);
-  }
-  printf("[+]Binding successfull.\n");
-
-  if(listen(sockfd, 10) == 0){
-		printf("[+]Listening....\n");
-	}else{
-		perror("[-]Error in listening");
-    exit(1);
+	
+	//Step 1: Construct a TCP socket to listen connection request
+	if ((listen_sock = socket(AF_INET, SOCK_STREAM, 0)) == -1 ){  /* calls socket() */
+		perror("\nError: ");
+		return 0;
+	}
+	
+	//Step 2: Bind address to socket
+	bzero(&server, sizeof(server));
+	server.sin_family = AF_INET;         
+	server.sin_port = htons(port_number);   /* Remember htons() from "Conversions" section? =) */
+	server.sin_addr.s_addr = htonl(INADDR_ANY);  /* INADDR_ANY puts your IP address automatically */   
+	if(bind(listen_sock, (struct sockaddr*)&server, sizeof(server))==-1){ /* calls bind() */
+		perror("\nError: ");
+		return 0;
+	}     
+	
+	//Step 3: Listen request from client
+	if(listen(listen_sock, BACKLOG) == -1){  /* calls listen() */
+		perror("\nError: ");
+		return 0;
 	}
 
-  addr_size = sizeof(new_addr);
-  new_sock = accept(sockfd, (struct sockaddr*)&new_addr, &addr_size);
-  write_file(new_sock);
-  printf("[+]Data written in the file successfully.\n");
+	struct stat st = {0};
 
-  return 0;
+	if (stat(STORAGE, &st) == -1) { //create storage if it not exist
+
+	    mkdir(STORAGE, 0755);
+	}
+	
+	//Step 4: Communicate with client
+	while(1){
+		//accept request
+		sin_size = sizeof(struct sockaddr_in);
+		if ((conn_sock = accept(listen_sock,( struct sockaddr *)&client, (unsigned int*)&sin_size)) == -1) 
+			perror("\nError: ");
+  
+		printf("You got a connection from %s\n", inet_ntoa(client.sin_addr) ); /* prints client's IP */
+		
+		//start conversation
+		while(1){
+			//receives message from client
+			if (stat(STORAGE, &st) == -1) { //create storage if it not exist
+			    mkdir(STORAGE, 0755);
+			}
+			char number[BUFF_SIZE];
+ 			char character[BUFF_SIZE];
+			bytes_received = recv(conn_sock, fileName, BUFF_SIZE-1, 0); //blocking
+			
+			if (bytes_received <= 0){
+				printf("\nConnection closed");
+				break;
+			}
+			else{
+				fileName[bytes_received] = '\0';
+
+				char name[100];
+				strcpy(name, STORAGE); // save file in storage
+				strcat(name, fileName); // file name
+				printf("File name is: %s\n", name);
+				if((fileptr = fopen(name, "rb")) != NULL) { // check if file exist
+					bytes_sent = send(conn_sock, EXISTEDFILE, strlen(EXISTEDFILE), 0);
+					if (bytes_sent <= 0){
+						printf("\nConnection closed");
+						break;
+					}
+					fclose(fileptr);
+					continue;
+				}
+				else {
+					fclose(fileptr);
+					bytes_sent = send(conn_sock, "OK", 20, 0); // if file Name valid
+					if (bytes_sent <= 0){
+						printf("\nConnection closed");
+						break;
+					}
+					bytes_received = recv(conn_sock, &filelen, 20, 0);
+					if (bytes_received <= 0){
+						printf("\nConnection closed");
+						break;
+					}
+					printf("file Uploading: %s, Size of file: %ld\n\n", fileName, filelen);
+
+					int sumByte = 0;
+					fileptr = fopen(name, "wb");
+					fileContent = (char*) malloc(BUFF_SEND * sizeof(char));
+					while(1) {
+						bytes_received = recv(conn_sock, fileContent, BUFF_SEND, 0);
+						if(bytes_received == 0) {
+							printf("Error: File tranfering is interupted\n\n");
+						}
+						sumByte += bytes_received;
+						fwrite(fileContent, bytes_received, 1, fileptr);
+						free(fileContent);
+						fileContent = (char*) malloc(BUFF_SEND * sizeof(char));
+						if(sumByte >= filelen) {
+							break;
+						}
+					}  // file content
+					if (bytes_received <= 0){
+						printf("\nConnection closed");
+						break;
+					}
+					
+					bytes_sent = send(conn_sock, "Successful transfering\n", 30, 0);
+					if (bytes_sent <= 0){
+						printf("\nConnection closed");
+						break;
+					}
+
+					fclose(fileptr);
+					free(fileContent);
+				}
+			}
+		}//end conversation
+		close(conn_sock);	
+	}
+	
+	close(listen_sock);
+	return 0;
 }
